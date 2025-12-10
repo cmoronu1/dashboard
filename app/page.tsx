125b0f9f4796eb65bf9c5195252555ee4c11c96f
cmoronu1 <cmoronu74@gmail.com>
2025-12-10 15:54:41 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { FilterBox } from "./components/filter-page";
 import {  SearchBar } from "./components/search-page";
+import { AllTask } from "./data/campaigns";
 
 export default function Home() {
+  const [data, setData] = useState(AllTask);
   return <div className="w-[70%] m-auto mt-5">
     <SearchBar/>
     <FilterBox/>
