a9e40386d381f2702c915937e15f448310d5793a
cmoronu1 <cmoronu74@gmail.com>
2025-12-11 14:06:29 -0500

    Add campaign button

--- app/components/campaign-column.tsx
+++ app/components/campaign-column.tsx
@@ -1,11 +1,12 @@
 import { CampaignCard } from "./campaign-card";
+import { AddCampaign } from "./add-campaign";
 
 interface CampaignColumnInterface {
   data: campaign[];
 }
 export function CampaignColumn({ data }: CampaignColumnInterface) {
   return (
-    <>
+    <div className="flex flex-col gap-3">
       <div className="text-[#939393] flex items-center gap-2 text-[0.8em]">
         <span>{data[0].status}</span>{" "}
         <span className="text-black border-0 bg-[#f5f5f5] w-4 rounded-[0.3rem] text-center">
@@ -16,7 +17,9 @@ export function CampaignColumn({ data }: CampaignColumnInterface) {
         <div key={member.title}>
           <CampaignCard campaign={member} />
         </div>
+        
       ))}
-    </>
+      <AddCampaign/>
+    </div>
   );
 }
