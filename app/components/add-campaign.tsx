d28c83482369a8fe9511a09d281eb5b4436c6e31
cmoronu1 <cmoronu74@gmail.com>
2025-12-16 15:28:15 -0500

    Update add-campaign.tsx

--- app/components/add-campaign.tsx
+++ app/components/add-campaign.tsx
@@ -25,6 +25,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { format } from "date-fns";
 
 interface AddCampaignInterface {
   data: campaign[];
@@ -47,18 +48,38 @@ export function AddCampaign({ data, setData }: AddCampaignInterface) {
     let filteredObject = {};
 
     filteredObject = Object.fromEntries(
-        Object.entries(form).filter(
-          ([Key, value]) =>
-            Key != "status" && Key != "progress" && Key != "users"
-        ))
+      Object.entries(form).filter(
+        ([Key, value]) =>
+          Key != "status" &&
+          Key != "progress" &&
+          Key != "users" &&
+          Key != "updatedAt"
+      )
+    );
 
     return !(
       Object.values(filteredObject).every((member: any) => member.length > 0) &&
-      Object.values(form.users).length > 2 && Object.values(form.progress)[0] != -1
+      Object.values(form.users).length > 2 &&
+      Object.values(form.progress)[0] != -1
     );
   }, [form]);
 
-  console.log(isDisabled, Object.values(form.users).length);
+  const dateToday = new Date();
+
+  function AddEntry() {
+    setData((p) => [...p, form]);
+    setForm((p) => ({
+      ...p,
+      logo: "",
+      title: "",
+      progress: -1,
+      startDate: "",
+      endDate: "",
+      updatedAt: "",
+      status: data[0].status,
+      users: [],
+    }));
+  }
   return (
     <AlertDialog>
       <AlertDialogTrigger>
@@ -75,68 +96,70 @@ export function AddCampaign({ data, setData }: AddCampaignInterface) {
           </AlertDialogDescription>
         </AlertDialogHeader>
         <div className="grid gap-4">
-          {[
-            "logo",
-            "title",
-            "progress",
-            "startDate",
-            "endDate",
-            "updatedAt",
-          ].map((member) => (
-            <div className="grid gap-3" key={member}>
-              <Label>{member}</Label>
-              <Input
-                type={
-                  member == "logo"
-                    ? "url"
-                    : member == "startDate" ||
-                      member == "endDate" ||
-                      member == "updatedAt"
-                    ? "date"
-                    : member == "progress"
-                    ? "number"
-                    : "text"
-                }
-                defaultValue={
-                  member == "logo"
-                    ? "https://example.com/logo1.png"
-                    : member == "progress" &&
-                      (data[0].status == "Draft" ||
-                        data[0].status == "Archived")
-                    ? 0
-                    : ""
-                }
-                disabled={
-                  member == "progress" &&
-                  (data[0].status == "Draft" || data[0].status == "Archived")
-                    ? true
-                    : false
-                }
-                onChange={(event) =>
-                  member == "logo"
-                    ? setForm((p) => ({ ...p, logo: event.target.value }))
-                    : member == "title"
-                    ? setForm((p) => ({ ...p, title: event.target.value }))
-                    : member == "progress"
-                    ? setForm((p) => ({
-                        ...p,
-                        progress: event.target.value as unknown as number,
-                      }))
-                    : member == "startDate"
-                    ? setForm((p) => ({
-                        ...p,
-                        startDate: event.target.value,
-                      }))
-                    : member == "endDate"
-                    ? setForm((p) => ({ ...p, endDate: event.target.value }))
-                    : setForm((p) => ({
-                        ...p,
-                        updatedAt: event.target.value,
-                      }))
-                }
-              />
-            </div>
-          ))}
+          {["logo", "title", "progress", "startDate", "endDate"].map(
+            (member) => (
+              <div className="grid gap-3" key={member}>
+                <Label>{member == "progress" ? member + " (%)" : member}</Label>
+                <Input
+                  type={
+                    member == "logo"
+                      ? "url"
+                      : member == "startDate" || member == "endDate"
+                      ? "date"
+                      : member == "progress"
+                      ? "number"
+                      : "text"
+                  }
+                  defaultValue={
+                    member == "logo"
+                      ? "https://example.com/logo1.png"
+                      : member == "progress" &&
+                        (data[0].status == "Draft" ||
+                          data[0].status == "Archived")
+                      ? 0
+                      : ""
+                  }
+                  disabled={
+                    member == "progress" &&
+                    (data[0].status == "Draft" || data[0].status == "Archived")
+                      ? true
+                      : false
+                  }
+                  placeholder={
+                    member == "progress"
+                      ? "Enter the percentage value e.g 50"
+                      : ""
+                  }
+                  onChange={(event) =>
+                    member == "logo"
+                      ? setForm((p) => ({
+                          ...p,
+                          logo: event.target.value,
+                        }))
+                      : member == "title"
+                      ? setForm((p) => ({ ...p, title: event.target.value }))
+                      : member == "progress"
+                      ? setForm((p) => ({
+                          ...p,
+                          progress: event.target.value as unknown as number,
+                        }))
+                      : member == "startDate"
+                      ? setForm((p) => ({
+                          ...p,
+                          startDate: event.target.value,
+                        }))
+                      : member == "endDate"
+                      ? setForm((p) => ({
+                          ...p,
+                          endDate: event.target.value,
+                          updatedAt: format(dateToday, "MMM dd, yyyy"),
+                        }))
+                      : ""
+                  }
+                />
+              </div>
+            )
+          )}
           <div className="grid gap-3">
             <Label>Status</Label>
             <Input defaultValue={data[0].status} disabled />
@@ -146,7 +169,7 @@ export function AddCampaign({ data, setData }: AddCampaignInterface) {
         </div>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction disabled={isDisabled}>
+          <AlertDialogAction onClick={AddEntry} disabled={isDisabled}>
             Save Changes
           </AlertDialogAction>
         </AlertDialogFooter>
